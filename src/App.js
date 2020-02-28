5e116c783547632d61d60d6ab0ce4f94c5fbae8c
benweizhu <benwei.zhu@exxonmobil.com>
2020-02-28 10:17:04 +0800

    try break it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Select } from 'antd';
+
 import './App.css';
 
 const { Option } = Select;
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
+        <div className="container">
           <Select
             mode="multiple"
             style={{ width: '100%' }}
